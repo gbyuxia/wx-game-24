c400fa707e791f9bcab754b5c422bda8d0e4ce6a
gbyuxia <19003664@qq.com>
2016-11-03 17:43:08 +0800

    配合小程序官方API修改，取消modal组件，改用wx:showModal显示2类反馈

--- pages/count/count.js
+++ pages/count/count.js
@@ -5,9 +5,7 @@ Page({
          countLine:[{firstNum:'',operator:'',nextNum:'',result:'',isCounted:false}],
          result:[],
          isFinised:false,
-         isSuccessed:false,
-         modalHidden:true,
-         modalHidden2:true,         
+         isSuccessed:false,       
          grade:'初级',
          total:0,
          score:{gameIndex:0,successNum:0,skipNum:0,failNum:0}
@@ -105,31 +103,95 @@ Page({
                         thisGradeNum = 3;
                      }
                      thisScore = thisGradeNum * Math.max(1,Math.ceil(2-(useTime-13)/6)) + oldData.total;
-                    if (oldData.score.gameIndex >9 && oldData.score.successNum >=7){                        
-                       this.setData({modalHidden2:false,total:oldData.total + thisScore}); 
-                    }else{
+                   
+                                          
+                       this.setData({total:oldData.total + thisScore}); 
+                    
                         //结果正确
                         this.setData({
                             countLine : newCountLine,
                             isFinished:true,
-                            modalHidden:false,
+                           // modalHidden:false,
                             isSuccessed:true,
                             'score.successNum':Number(oldData.score.successNum) +1,
                             thisUnitTime:useTime,
                             total: thisScore
-                        })
-                    }
+                        })                        
+                    
                 }else{
                     //结果错误
                     this.setData({
                         countLine : newCountLine,
                         isFinished:true,
-                        modalHidden:false,
+                        //modalHidden:false,
                         isSuccessed:false,
                         'score.failNum':Number(oldData.score.failNum) +1,
                         thisUnitTime:useTimeTxt
                     })
                 }
+                //每局完成的反馈
+                if (oldData.score.gameIndex >9 && oldData.score.successNum >=8){ 
+                       let that=this, btnTxt= oldData.grade=='高级'?'看看总成绩':'下一等级',cnt = '';
+                       if (oldData.grade=='高级'){
+                           cnt="最难的一关你已经赢了"+(oldData.score.successNum+1) +"局，这么厉害，你咋不上天呢？"       
+                       }else{
+                           cnt="您已经玩了10轮"+oldData.grade +"级别的游戏，胜率超过80%，这么厉害，赶紧去玩下个等级的游戏吧！"
+                       }
+                       wx.showModal({
+                            content: cnt,
+                            confirmText:btnTxt ,
+                            showCancel:false,
+                            success: function(res) {
+                                if (res.confirm) {
+                                    if ( that.data.grade=='高级'){
+                                        wx.navigateTo({ url: '../result/result?total='+that.data.total+'&user='+that.data.userInfo});
+                                    }else{
+                                        let newGrade = that.data.grade=="初级"?'中级':'高级';      
+                                        that.setData({grade:newGrade});  
+                                        that.creatUnit();
+                                        that.setData({score:{gameIndex:0,successNum:0,skipNum:0,failNum:0}})
+                                    }
+                                }
+                            }
+                        })
+                    }else{
+                let that = this,cnt = '',tt='可惜，不对哦！';
+                        if(Number(r) == 24){
+                            if(useTime<=18){
+                                cnt='天才，受我一拜！'
+                            }else if(useTime<=53){
+                                cnt='您真是才思敏捷！'
+                            }else if(useTime<=70){
+                                cnt='争取更快点，加油！' 
+                            }else{
+                                cnt='脑子生锈了吧？那更需要动动咯，加油'
+                            }
+                            tt='祝贺你，成功了！'    
+                        }
+                        
+                        cnt+="你用了"+useTime+"秒完成这题的计算。"
+                        wx.showModal({
+                            title: tt,
+                            content: cnt,
+                            confirmText:"下一题" ,
+                            cancelText:"重新算一次" ,
+                            success: function(res) {
+                                if (res.confirm) {
+                                    that.creatUnit();
+                                }else{
+                                    that.setData(
+                                        {
+                                            disabled:[false,false,false,false,false,false],
+                                            countLine:[{firstNum:'',operator:'',nextNum:'',result:'',isCounted:false}],
+                                            isFinised:false,
+                                            isSuccessed:false,
+                                            modalHidden:true
+                                        }
+                                    )
+                                }
+                            }
+                        })
+                    }
             }else{
                 //计算未完成，生成新一行空的算式列式
                  newCountLine.push({firstNum:'',nextNum:'',isCounted:false});
@@ -154,18 +216,5 @@ Page({
     toSkip(){
         this.setData({'score.skipNum':Number(this.data.score.skipNum) +1});
         this.creatUnit();
-    },
-    beginNextGrade(){
-        if ( this.data.grade=='高级'){
-             wx.navigateTo({ url: '../result/result?total='+this.data.total+'&user='+this.data.userInfo});
-        }else{
-            let newGrade = this.data.grade=="初级"?'中级':'高级';      
-            this.setData({grade:newGrade});  
-            this.creatUnit();
-            this.setData({score:{gameIndex:0,successNum:0,skipNum:0,failNum:0},modalHidden2:true})
-        }
-
-       
     }
-
 });
